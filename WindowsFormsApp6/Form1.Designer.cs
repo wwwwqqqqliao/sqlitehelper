d51db0edac6de546e3dcf5f9ce1d2e42d4b4a86d
Administrator <Administrator@MM-202212051155>
2024-01-02 15:23:56 +0800

    添加项目文件。

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace sqlitehelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(112, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 38);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "增加";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(112, 95);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 38);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "删除";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(112, 154);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(129, 38);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "修改";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 283);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace sqlitehelper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      
+        SQLiteHelper h;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            h=new SQLiteHelper();
+           // SQLiteHelper.CreateDB("alarm.db");//创建数据库
+            SQLiteHelper.SetConnectionString("alarm.db");//创建连接字符串
+
+
+            try
+            {
+                h.ExecuteNonQuery("create table a1(id int, name text,age int)", null);//创建a1表
+            }
+            catch (Exception)
+            {
+
+               
+            }
+           
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)//增加
+        {
+            h.ExecuteNonQuery("insert into  a1 values(5,'q1',20)", null);
+        }
+
+        private void button4_Click(object sender, EventArgs e)//删除
+        {
+            h.ExecuteNonQuery("delete from  a1 where id=5", null);
+        }
+
+        private void button5_Click(object sender, EventArgs e)//修改
+        {
+            h.ExecuteNonQuery(" update  a1 set age=40 where id=4", null);
+        }
+    }
+}
